9065e801e723623f37bb363b0771c1db29b72754
prajjwalkumar17 <write2prajjwal@gmail.com>
2025-02-12 09:14:33 +0530

    nixos: with pckgs that are required by hyperpland

---
+++ configuration.nix
@@ -0,0 +1,213 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./modules/nvidia.nix
+    ];
+
+  # Bootloader.
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      systemd-boot.configurationLimit = 5;
+      timeout = 3;
+    };
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Select internaXFCEtionalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the sddm Desktop Environment.
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.hangsai = {
+    isNormalUser = true;
+    description = "hangsai";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = "/run/current-system/sw/bin/zsh";
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allwInsecure = true;
+  # nixpkgs.config.permittedInsecurePackages = [
+  #	"python-2.7.18.8"
+  # ];
+
+  # Fonts
+  fonts.fontDir.enable = true;
+  fonts.fontconfig.enable = true;
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+
+    cargo  
+    kitty
+    python3Packages.virtualenv
+    python3
+    rustup
+    vim
+    wget
+
+    # Hyperland
+    bibata-cursors
+    brightnessctl
+    bluez
+    bluez-alsa
+    bluez-tools
+    bluetui
+    dunst
+    grim
+    libnotify
+    networkmanagerapplet
+    pulseaudio
+    pipewire
+    rofi-wayland
+    swaynotificationcenter
+    swaylock
+    swww
+    wireplumber
+    wofi
+    waybar
+    (pkgs.waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+    }))
+
+    # NVIM
+    clang
+    cmake
+    fzf
+    gcc
+    gnumake
+    libtool
+    neovim
+    pkg-config
+    ripgrep
+    vimPlugins.telescope-fzf-native-nvim
+
+    # fonts
+    (pkgs.nerdfonts.override {
+      fonts = [
+        "IBMPlexMono"
+	"Iosevka"
+	"IosevkaTerm"
+	"JetBrainsMono"
+      ];
+    })
+
+    
+    # dev utilities
+    git
+    meslo-lgs-nf
+    zoxide
+    zsh
+    zsh-powerlevel10k
+
+    # utilities
+    unzip
+
+    # media
+    spotify
+  ];
+  programs.thunar.enable = true;
+  programs.hyprland.enable = true;
+  environment.variables = {
+    XCURSOR_THEME = "Bibata-Modern-Classic";
+    XCURSOR_SIZE = "24";
+  };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
